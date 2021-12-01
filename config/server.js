3aebc28a51fa61842019ce74eb838936657d889c
amrgasser <amr_gasser@hotmail.com>
2021-12-01 16:36:47 +0200

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: {
+    enabled: true
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4c131dcae993315d049f8035a20d3464'),
