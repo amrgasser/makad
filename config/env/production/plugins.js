6f5c88952a2835e98810a77f53172db291419d96
amrgasser <amr_gasser@hotmail.com>
2021-12-20 14:31:43 +0200

    Production env

---
+++ config/env/production/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME', 'doh4vcgwz'),
+      api_key: env('CLOUDINARY_KEY', '266441192261964'),
+      api_secret: env('CLOUDINARY_SECRET', 'zXImiFpwDx2BwYAiwAZqB6ZVm2o'),
+    },
+  },
+  // ...
+});
