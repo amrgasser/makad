c85c1e83db9a7b27fd2780fda8bf376204e612fc
amrgasser <amr_gasser@hotmail.com>
2021-12-29 13:47:34 +0200

    Home and Home at Makadi

--- api/home-at-makadi-heights/controllers/home-at-makadi-heights.js
+++ api/home-at-makadi-heights/controllers/home-at-makadi-heights.js
@@ -5,4 +5,18 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  masterPlan: async (ctx) => {
+    let amenities; let zones;
+    if (ctx.query._q) {
+      zones = await strapi.services.zones.search(ctx.query);
+      amenities = await strapi.services.amenity.search(ctx.query);
+    } else {
+      zones = await strapi.services.zones.find(ctx.query);
+      amenities = await strapi.services.amenity.find(ctx.query);
+    }
+    let masterPlan = { zones, amenities }
+    return masterPlan;
+  }
+};
