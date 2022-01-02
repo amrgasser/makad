7b9ba1ae5ecaf0c6dc2d37060107a9576f09d4df
amrgasser <amr_gasser@hotmail.com>
2022-01-02 13:17:19 +0200

    Heroku push

--- api/home-at-makadi-heights/controllers/home-at-makadi-heights.js
+++ api/home-at-makadi-heights/controllers/home-at-makadi-heights.js
@@ -1,14 +1,10 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
 
 module.exports = {
-
   masterPlan: async (ctx) => {
-    let amenities; let zones;
+    let amenities;
+    let zones;
     if (ctx.query._q) {
       zones = await strapi.services.zones.search(ctx.query);
       amenities = await strapi.services.amenity.search(ctx.query);
