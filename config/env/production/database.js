1f6ca4f7b7cdd108d7e4ab5c03e77f5bb432103c
amrgasser <amr_gasser@hotmail.com>
2022-01-26 18:10:45 +0200

    DB

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,23 @@
-// // const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {
-        ssl: false,
-      },
+      options: {},
     },
   },
 });
